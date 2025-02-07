2295c063962c2ba127c511d3a55e165d2aa064bd
Chaitanya_1972 <84122021+EDThomasdave@users.noreply.github.com>
2025-02-07 20:05:47 +0530

    Delete confirm.js

--- confirm.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('confirmNoButton').addEventListener('click', function() {
-    window.location.href = 'reason.html';
-});
-
-document.getElementById('goBackButton').addEventListener('click', function() {
-    window.location.href = 'index.html';
-});
