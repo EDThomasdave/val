41a3c16503432fed8daf0f24d23de47d9e88b81c
Chaitanya_1972 <84122021+EDThomasdave@users.noreply.github.com>
2025-02-07 20:07:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('yesButton').addEventListener('click', function() {
-    window.location.href = 'yes.html';
-});
-
-document.getElementById('noButton').addEventListener('click', function() {
-    window.location.href = 'confirm.html';
-});
