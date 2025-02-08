cd1097dce4087fa3b8cba01b6277f53e81205dc1
Chaitanya_1972 <84122021+EDThomasdave@users.noreply.github.com>
2025-02-08 09:29:31 +0530

    Update script.js

--- valentine-proposal/script.js
+++ valentine-proposal/script.js
@@ -1,5 +1,5 @@
 document.getElementById("yesButton").addEventListener("click", function () {
-    window.location.href = "http://localhost:3000/response?answer=Yes";
+    window.location.href = "http://tanubaby.site/yes.html";
 });
 
 document.getElementById("noButton").addEventListener("click", function () {
